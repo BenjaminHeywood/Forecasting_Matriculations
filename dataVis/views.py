e437c7e7db4ea41a1c86eca7e2c83f16c32487cb
BenHeywood <128393423+BenjaminHeywood@users.noreply.github.com>
2026-06-07 17:09:13 +0100

    Fix Django app setup and templates

--- dataVis/views.py
+++ dataVis/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+results = [
+    {
+        'category': 'UG',
+        'region': 'Africa', 
+        'controlled': 'Yes', 
+        'fee_status': 'Overseas',
+        'date': '2026-06-07', 
+        'predicted_matrics': '35'
+    },
+        {
+        'category': 'UG',
+        'region': 'North America', 
+        'controlled': 'Yes', 
+        'fee_status': 'Overseas',
+        'date': '2026-06-07',
+        'predicted_matrics': '15'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Home</h>')
+    context = { 
+        'results': results
+    }
+    return render(request, 'dataVis/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About</h>')
+    return render(request, 'dataVis/about.html')
