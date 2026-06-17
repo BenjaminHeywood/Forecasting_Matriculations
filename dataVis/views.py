3e0f44692089ee7f1d27aa154c8674a2d36e894e
BenjaminPHeywood <benjaminpheywood@gmail.com>
2026-06-17 12:11:08 +0100

    Updating with dummy graphs

--- dataVis/views.py
+++ dataVis/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render
+import pandas as pd
+import json
 
-results = [
-    {
-        'category': 'UG',
-        'region': 'Africa', 
-        'controlled': 'Yes', 
-        'fee_status': 'Overseas',
-        'date': '2026-06-07', 
-        'predicted_matrics': '35'
-    },
-        {
-        'category': 'UG',
-        'region': 'North America', 
-        'controlled': 'Yes', 
-        'fee_status': 'Overseas',
-        'date': '2026-06-07',
-        'predicted_matrics': '15'
-    }
-]
+
+summary_course = pd.read_csv(r"C:\Users\bheywood\OneDrive - University of Dundee\Desktop\Dissertation\summary_course.csv")
+summary_region = pd.read_csv(r"C:\Users\bheywood\OneDrive - University of Dundee\Desktop\Dissertation\summary_region.csv")
+
+
+# results = [
+#     {
+#         'category': 'UG',
+#         'region': 'Africa', 
+#         'controlled': 'Yes', 
+#         'fee_status': 'Overseas',
+#         'date': '2026-06-07', 
+#         'predicted_matrics': '35'
+#     },
+#         {
+#         'category': 'UG',
+#         'region': 'North America', 
+#         'controlled': 'Yes', 
+#         'fee_status': 'Overseas',
+#         'date': '2026-06-07',
+#         'predicted_matrics': '15'
+#     }
+# ]
+
 
 def home(request):
-    context = { 
-        'results': results
+    context = {
+        'summary_course': summary_course.to_dict(orient="records"),
+        'summary_region': summary_region.to_dict(orient="records"),
+        'summary_course_json': json.dumps(summary_course.to_dict(orient="records")),
+        'summary_region_json': json.dumps(summary_region.to_dict(orient="records")),
     }
     return render(request, 'dataVis/home.html', context)
 
